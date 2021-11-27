1ea20abb6beae7a8c676190d670b6c1ee45233c7
Athya <athyauk@gmail.com>
2021-11-26 22:52:31 -0800

    Removed wrong client ID

--- src/front/src/spotify.js
+++ src/front/src/spotify.js
@@ -1,5 +1,5 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
-const clientId = "fbbb7c8a525e45a8b1e47c28f83535b3";
+const clientId = "9cf53a8f93444cadac7b3e6d990a9e6d";
 const scopes = ["user-top-read", "user-read-email"];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&scope=${scopes.join(

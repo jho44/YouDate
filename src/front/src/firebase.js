ab93d010b9a4f027c31a8b4dc26335b23b365f39
Jessica Ho <hojessica1230@gmail.com>
2021-11-27 22:59:33 -0800

    firebase setup

---
+++ src/front/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKiyluRRzKamxd8NgFmYduWxerInedvR0",
+  authDomain: "datify-49ddc.firebaseapp.com",
+  projectId: "datify-49ddc",
+  storageBucket: "datify-49ddc.appspot.com",
+  messagingSenderId: "426040737519",
+  appId: "1:426040737519:web:68c4717f117fff6344651a",
+  measurementId: "G-2D806J8GZC"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
